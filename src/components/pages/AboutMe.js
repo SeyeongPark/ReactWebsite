e671031d165a731d99d9db7e52f37d29847b1e01
SeyeongPark <lynn.yeong.102@gmail.com>
2022-06-09 00:45:59 -0400

    put my photo

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -11,10 +11,23 @@ export default function AboutMe(){
                 <div class="text-main">
                 <h1>About.</h1>
                     <p>I'm a <em>web developer</em> in Toronto, Canada. I like making interesting with code. I'm also love a journey together with code.</p>
+                    <br/>
+                    <p>I can describe my self..</p>
+                    <div class="myself">
+                        <div class="myself-left">
+                            <p>Creative thinker</p>
+                        </div>
+                        <div class="myself-mid">
+                            <p>Enjoying learning new</p>
+                        </div>
+                        <div class="myself-right">
+                            <p>Focusing on tasks</p>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="img-main">
-                    <img class="img-logo" src="../images/avatar.PNG" />
+                    <img class="img-logo" src="../images/me-3.JPEG" />
                 </div>
             </div>
             
