c6b0692a896ce6d51aa6cad505c38b6a03d13e00
SeyeongPark <lynn.yeong.102@gmail.com>
2022-06-24 19:55:45 -0400

    remoce lgoo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,9 +24,9 @@ function Navbar() {
     <>
         <nav className="navbar">
             <div className='navbar-container'>
-                <Link to="/" className="navbar-logo">
+                {/* <Link to="/" className="navbar-logo">
                     SeyeongPark <i class="fab fa-typo3"/>
-                </Link>
+                </Link> */}
                 <div className='menu-icon' onClick={handleClick}>
                   <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
                 </div>
