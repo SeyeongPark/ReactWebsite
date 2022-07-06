0802decea7e091209bbea0f3d6d991ab02e64a14
SeyeongPark <lynn.yeong.102@gmail.com>
2022-07-06 10:30:27 -0400

    move scroll down

--- src/components/CoverSection.js
+++ src/components/CoverSection.js
@@ -9,7 +9,7 @@ function CoverSection() {
       <h3>Seyeong Park</h3>
       <p>Web Developer</p>
       <section id="scroll" class="demo">
-        <a href="#aboutme"><span></span>Scroll</a>
+        <a href="#aboutme"><span></span>Scroll Down</a>
       </section>
     </div>
   )
