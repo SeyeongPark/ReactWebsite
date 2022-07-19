8cc7870b31f2f1844d81a840c410db80d5ee5b2f
SeyeongPark <lynn.yeong.102@gmail.com>
2022-07-19 00:48:35 -0400

    light schema

--- src/components/CoverSection.js
+++ src/components/CoverSection.js
@@ -5,7 +5,7 @@ import './CoverSection.css';
 function CoverSection() {
   return (
     <div className='cover-container'>
-      <img className="cover-img" src="/images/banner-navy-1.png" alt="cover-image"/>
+      <img className="cover-img" src="/images/banner-navy.png" alt="cover-image"/>
       <h3>Seyeong Park</h3>
       <p>Web Developer</p>
       <section id="scroll" class="demo">
