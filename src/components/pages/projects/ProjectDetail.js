2a2fb5ccb3f54f6092421ab7727e76a1480eba91
SeyeongPark <lynn.yeong.102@gmail.com>
2022-07-19 21:50:39 -0400

    create project detail templete page

--- src/components/pages/projects/ProjectDetail.js
+++ src/components/pages/projects/ProjectDetail.js
@@ -3,14 +3,6 @@ import './Project.css'
 export default function ProjectDetail() {
     return (
         <div className="container">
-            <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
-            <script
-            src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
-            crossorigin></script>
-            <script
-            src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-            crossorigin></script>
-
             <h1>Title</h1>
             <div className='sum-detail'>
                 <div className='sum-team-solo'>
