1c7a869f32259087db3ca6193a148d5b42381642
SeyeongPark <lynn.yeong.102@gmail.com>
2022-07-23 13:14:08 -0400

    add profile pic

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -41,7 +41,7 @@ export default function AboutMe(){
                         </div>
 
                     <div className="img-main">
-                        {/* <img className="img-me" src="../images/me-4.png"/> */}
+                        <img className="img-me" src="../images/me-5.png"/>
                     </div>
 
                     <div className='scroll-container'>
