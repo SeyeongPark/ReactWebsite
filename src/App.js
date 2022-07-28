d7410db7c061bd41f32d42be8d966d26967006e7
SeyeongPark <lynn.yeong.102@gmail.com>
2022-07-28 13:41:31 -0400

    test speed of web performance

--- src/App.js
+++ src/App.js
@@ -8,49 +8,49 @@ import Footer from './components/Footer';
 
 function App() {
   // project detail
-  const todo = {
-    title : 'Todo',
-    coop : 'Solo',
-    skill : 'MongoDB, Express, React, Node.js, HTML, CSS',
-    date : 'June 2022',
-    url : 'https://sptech-todo.netlify.app',
-    github : 'https://github.com/SeyeongPark/todo-mern-backend', 
-    description : "A simple web application helps users stay organized and managed by adding their tasks to lists.</br>In this application, the user can check all of the items on the list and create, delete, and make each to-do item to the 'completed' state.</br></br> Also, user can access this application not only on their local computer but also everywhere that have an internet connection by using user authentication.<div className='img-container'><img id='detail-img' src='../images/projects/todo/td-detail.png'/></div>",
-    learned: '- Developing CRUD operation with the API server with MongoDB</br>- Hosting the back-end (server) side and connecting it with the front-end(client) side</br>- Styling advanced UI/UX with JSX(using a conditional operator) and CSS (using root, hover..)</br>- Setting authentication with a json web token'
-  }
+  // const todo = {
+  //   title : 'Todo',
+  //   coop : 'Solo',
+  //   skill : 'MongoDB, Express, React, Node.js, HTML, CSS',
+  //   date : 'June 2022',
+  //   url : 'https://sptech-todo.netlify.app',
+  //   github : 'https://github.com/SeyeongPark/todo-mern-backend', 
+  //   description : "A simple web application helps users stay organized and managed by adding their tasks to lists.</br>In this application, the user can check all of the items on the list and create, delete, and make each to-do item to the 'completed' state.</br></br> Also, user can access this application not only on their local computer but also everywhere that have an internet connection by using user authentication.<div className='img-container'><img id='detail-img' src='../images/projects/todo/td-detail.png'/></div>",
+  //   learned: '- Developing CRUD operation with the API server with MongoDB</br>- Hosting the back-end (server) side and connecting it with the front-end(client) side</br>- Styling advanced UI/UX with JSX(using a conditional operator) and CSS (using root, hover..)</br>- Setting authentication with a json web token'
+  // }
 
-  const emp = {
-    title : 'Employee Time Tracker',
-    coop : 'Solo',
-    skill : 'Java, MySQL, Spring Framework, HTML, CSS',
-    date : 'May 2022',
-    url : 'https://timestamp-app-1.herokuapp.com',
-    github : 'https://github.com/SeyeongPark/PartTime-TimeStamp', 
-    description : "A web application that records employees' working time stamps from start to finish, and allows the manager to access all information. </br></br>In this application, there are 2 main roles, employee and manager. All workers can record their working hours by clicking the 'record' button and will be calculated their working hours based on starting to finishing work on the main page. On the 'My Employee' page, a manager can check not only all employees' information for working hours, but also update their salary and specific position.<div className='img-container'><img id='detail-img' src='../images/projects/timetracker/tt-detail.png'/></div>",
-    learned: '- Structuring SQL module and entity for a database using MySQL</br>- Implementing Controller for all features integrating with Restful API</br>- Setting the authentication to log in and permission for a manager page'
-  }
+  // const emp = {
+  //   title : 'Employee Time Tracker',
+  //   coop : 'Solo',
+  //   skill : 'Java, MySQL, Spring Framework, HTML, CSS',
+  //   date : 'May 2022',
+  //   url : 'https://timestamp-app-1.herokuapp.com',
+  //   github : 'https://github.com/SeyeongPark/PartTime-TimeStamp', 
+  //   description : "A web application that records employees' working time stamps from start to finish, and allows the manager to access all information. </br></br>In this application, there are 2 main roles, employee and manager. All workers can record their working hours by clicking the 'record' button and will be calculated their working hours based on starting to finishing work on the main page. On the 'My Employee' page, a manager can check not only all employees' information for working hours, but also update their salary and specific position.<div className='img-container'><img id='detail-img' src='../images/projects/timetracker/tt-detail.png'/></div>",
+  //   learned: '- Structuring SQL module and entity for a database using MySQL</br>- Implementing Controller for all features integrating with Restful API</br>- Setting the authentication to log in and permission for a manager page'
+  // }
 
-  const hotauc = {
-    title : 'HotAuc Action',
-    coop : 'Team',
-    skill : 'JavaScript, React.js, MongoDB, HTML, CSS',
-    date : 'APRIL 2022',
-    description : "HotAuc is a web application where Hot Wheels collectors can buy and sell cars over auctions. Moreover, the application allows buyers to pay for a winning bid over Paypal, sellers to check their earnings and administrators to edit information.<div className='img-container'><img id='detail-img' src='../images/projects/hotauc/pages.png'/></div>",
-    contribution : '- Implemented a user interface for results of the payment based on returned data from Payment API (PayPal)<br/>- Showed the users’ selling/ buying information (total earnings, each information of items..), which is stored on MongoDB<br/>- Designed UX/UI of user dashboard frame',
-    learned: "- Understanding React.js and MongoDB (module, schema, api..)  </br>- Communicating with team (8) members and Reporting my worked tasks during the meeting </br>- Reading and understanding other team members's code"
-  }
+  // const hotauc = {
+  //   title : 'HotAuc Action',
+  //   coop : 'Team',
+  //   skill : 'JavaScript, React.js, MongoDB, HTML, CSS',
+  //   date : 'APRIL 2022',
+  //   description : "HotAuc is a web application where Hot Wheels collectors can buy and sell cars over auctions. Moreover, the application allows buyers to pay for a winning bid over Paypal, sellers to check their earnings and administrators to edit information.<div className='img-container'><img id='detail-img' src='../images/projects/hotauc/pages.png'/></div>",
+  //   contribution : '- Implemented a user interface for results of the payment based on returned data from Payment API (PayPal)<br/>- Showed the users’ selling/ buying information (total earnings, each information of items..), which is stored on MongoDB<br/>- Designed UX/UI of user dashboard frame',
+  //   learned: "- Understanding React.js and MongoDB (module, schema, api..)  </br>- Communicating with team (8) members and Reporting my worked tasks during the meeting </br>- Reading and understanding other team members's code"
+  // }
 
-  const serveymaker = {
-    title : 'Super Survey Maker',
-    coop : 'Team',
-    skill : 'HTML, CSS',
-    date : 'DECEMBER 2020',
-    url : 'https://team1c-f2020.herokuapp.com',
-    github : 'https://github.com/jodoubleny/team1c', 
-    description : "A web application where users can create their own surveys and manage the surveys.<br/><br/>A logged-in user is able to create a survey based on the survey template. All of the surveys will show on the ‘List of surveys’ page. Through the ‘My Survey’ page, the user can check the user’s survey and edit/delete the survey.<div className='img-container'><img id='detail-img' src='../images/projects/surveymaker/pages.png'/><img id='detail-img' src='../images/projects/surveymaker/detail-sm.png'/></div>",
-    contribution : '- Designed UX/UI  for all web pages as a Web Designer<br/>- Documented an External Design Document (Style Guide) which is included wireframes for each page, colour swatches, mockups, navigation schematic, and so on',
-    learned: '- Understanding styling for HTML and CSS</br>- Communicating with team members and Reporting my worked tasks during the meeting'
-  }
+  // const serveymaker = {
+  //   title : 'Super Survey Maker',
+  //   coop : 'Team',
+  //   skill : 'HTML, CSS',
+  //   date : 'DECEMBER 2020',
+  //   url : 'https://team1c-f2020.herokuapp.com',
+  //   github : 'https://github.com/jodoubleny/team1c', 
+  //   description : "A web application where users can create their own surveys and manage the surveys.<br/><br/>A logged-in user is able to create a survey based on the survey template. All of the surveys will show on the ‘List of surveys’ page. Through the ‘My Survey’ page, the user can check the user’s survey and edit/delete the survey.<div className='img-container'><img id='detail-img' src='../images/projects/surveymaker/pages.png'/><img id='detail-img' src='../images/projects/surveymaker/detail-sm.png'/></div>",
+  //   contribution : '- Designed UX/UI  for all web pages as a Web Designer<br/>- Documented an External Design Document (Style Guide) which is included wireframes for each page, colour swatches, mockups, navigation schematic, and so on',
+  //   learned: '- Understanding styling for HTML and CSS</br>- Communicating with team members and Reporting my worked tasks during the meeting'
+  // }
 
   return (
     <>
@@ -59,10 +59,10 @@ function App() {
       <Switch>
         <Route path='/' exact component={Home}/>
         <Route path='/project' exact component={ProjectDetail}/>
-        <Route path='/project/todo' render={() => <ProjectDetail {...todo} />}/>
+        {/* <Route path='/project/todo' render={() => <ProjectDetail {...todo} />}/>
         <Route path='/project/timetracker' render={() => <ProjectDetail {...emp} />}/>
         <Route path='/project/hotauc' render={() => <ProjectDetail {...hotauc} />}/>
-        <Route path='/project/surveymaker' render={() => <ProjectDetail {...serveymaker} />}/>
+        <Route path='/project/surveymaker' render={() => <ProjectDetail {...serveymaker} />}/> */}
       </Switch>
     </Router>
       <Footer/>
