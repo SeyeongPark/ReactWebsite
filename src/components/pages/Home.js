86150d2212aad398db6692f63b0dc73c053fc6eb
SeyeongPark <lynn.yeong.102@gmail.com>
2022-07-31 22:21:23 -0400

    test speed of web performance

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,10 +4,6 @@ import AboutMe from './AboutMe.js';
 import SkillSets from './SkillSets.js';
 import ProjectCards from '../ProjectCards'
 
-// const AboutMe = React.lazy(() => import('./AboutMe.js'));
-// const SkillSets = React.lazy(() => import('./SkillSets.js'));
-// const ProjectCards = React.lazy(() => import('../ProjectCards.js'));
-
 class Home extends Component {
     render(){
         return(
