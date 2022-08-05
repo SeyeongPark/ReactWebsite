716bdd942a91a1a22c3d2e9a9720632949cc84bc
SeyeongPark <lynn.yeong.102@gmail.com>
2022-08-05 19:07:17 -0400

    test with adding navidation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import React, { Suspense, lazy } from 'react';
-import { Navbar } from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react';
 import './App.css';
-import Home from './components/pages/Home'
-import { ProjectDetail } from './components/pages/ProjectDetail'
-import { Footer } from './components/Footer';
-import projectDetail from './components/pages/projectDetail.json'
+import Navigation from './components/pages/Navigation';
 
 function App() {
 
   return (
     <>
-    <Router>
-      <Navbar/> 
-      <Switch>
-        <Route path='/' exact component={Home}/>
-        <Route path='/project/todo' render={() => <ProjectDetail {...projectDetail.todo} />}/>
-        <Route path='/project/timetracker' render={() => <ProjectDetail {...projectDetail.emp} />}/>
-        <Route path='/project/hotauc' render={() => <ProjectDetail {...projectDetail.hotauc} />}/>
-        <Route path='/project/surveymaker' render={() => <ProjectDetail {...projectDetail.serveymaker} />}/>
-      </Switch>
-    </Router>
-      <Footer/>
+    <Navigation/>
     </>
   );
 }
---
+++ src/components/pages/Navigation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Navbar } from '../Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Home'
+import { ProjectDetail } from './ProjectDetail'
+import { Footer } from '../Footer';
+import projectDetail from './projectDetail.json'
+
+function Navigation() {
+
+  return (
+    <>
+    <Router>
+      <Navbar/> 
+      <Switch>
+        <Route path='/' exact component={Home}/>
+        <Route path='/project/todo' render={() => <ProjectDetail {...projectDetail.todo} />}/>
+        <Route path='/project/timetracker' render={() => <ProjectDetail {...projectDetail.emp} />}/>
+        <Route path='/project/hotauc' render={() => <ProjectDetail {...projectDetail.hotauc} />}/>
+        <Route path='/project/surveymaker' render={() => <ProjectDetail {...projectDetail.serveymaker} />}/>
+      </Switch>
+    </Router>
+      <Footer/>
+    </>
+  );
+}
+
+export default Navigation;
